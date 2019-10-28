d25566dadb4dd40937808dd305df9d7e17699a22
Jazmin M <jazminmerckel@gmail.com>
2019-10-28 16:59:12 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -18,8 +18,8 @@ class Customer
     @@all
   end
   
-  def new_meal(waiter, total, tip)
-    Meal.new(waiter, total, tip)
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
   end
   
   def meals 

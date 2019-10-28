f0b1fa76cd039a9f95d624aa15d4b3d9bb43944e
Jazmin M <jazminmerckel@gmail.com>
2019-10-28 16:35:51 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,10 +2,15 @@ class Customer
   
   attr_accessor :name, :age
   
+  @@all = []
+  
   def initialize(name, age)
     @name = name
     @age = age
   end
   
+  def self.all
+    @@all
+  end
   
 end
